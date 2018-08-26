08fbf84d50a2374a922fc4475a027cd448d95bad
EvenZZZ <ezhu0915@163.com>
2018-08-26 18:37:11 +0800

    整理

--- Demo/scripts/global.js
+++ Demo/scripts/global.js
@@ -33,132 +33,17 @@ function addClass(element,value) {
   }
 }
 
-function highlightPage() {
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
-  var links = document.getElementsByTagName("nav")[0].getElementsByTagName("a");
-  for (var i=0; i<links.length; i++) {
-    var linkurl = links[i].getAttribute("href");
-    var currenturl = window.location.href;
-    if (currenturl.indexOf(linkurl) != -1) {
-      links[i].className = "here";
-      var linktext = links[i].lastChild.nodeValue.toLowerCase();
-      document.body.setAttribute("id",linktext);
-    }
-  }
-}
 
-function moveElement(elementID,final_x,final_y,interval){
-  if (!document.getElementById) return false;
-  if (!document.getElementsByTagName) return false;
-  var elem = document.getElementById(elementID);
-  if (elem.movement) {
-    clearTimeout(elem.movement);
-  }
-  if (!elem.style.left){
-    elem.style.left = "0px";
-  }
-  if (!elem.style.top){
-    elem.style.top = "0px";
-  }
-  var xpos = parseInt(elem.style.left);
-  var ypos = parseInt(elem.style.top);
-  if (xpos == final_x && ypos == final_y) {
-    return true;
-  }
-  if (xpos < final_x){
-    var dist = Math.ceil((final_x - xpos)/10);
-    xpos = xpos + dist;
-  }
-  if (xpos > final_x){
-    var dist = Math.ceil((xpos - final_x)/10);
-    xpos = xpos - dist
-  }
-  if (ypos < final_y){
-    var dist = Math.ceil((final_y - ypos)/10);
-    ypos = ypos + dist;
-  }
-  if (ypos > final_y){
-    var dist = Math.ceil((ypos - final_y)/10);
-    ypos = ypos - dist
-  }
-  elem.style.left = xpos + "px";
-  elem.style.top = ypos + "px";
-  var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")"
-  elem.movement = setTimeout(repeat,interval);
-}
-
-function prepareSlideshow(){
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
-  if (!document.getElementById("intro")) return false;
-  var intro = document.getElementById("intro");
-  var slideshow = document.createElement("div");
-  slideshow.setAttribute("id","slideshow");
-  var preview = document.createElement("img");
-  preview.setAttribute("src","images/Eason_all.jpg");
-  preview.setAttribute("alt","a glimpse of what awaits you");
-  preview.setAttribute("id","preview");
-  slideshow.appendChild(preview);
-  insertAfter(slideshow,intro);
-
-  var links = document.getElementsByTagName("a");
-  var destination;
-  for (var i=0; i<links.length; i++){
-    links[i].onmouseover = function(){
-      destination = this.getAttribute("href");
-      if (destination.indexOf("index.html") != -1){
-        moveElement("preview",0,0,5);
-      }
-      if (destination.indexOf("about.html") != -1){
-        moveElement("preview",-180,0,5);
-      }
-      if (destination.indexOf("photos.html") != -1){
-        moveElement("preview",-360,0,5);
-      }
-      if (destination.indexOf("live.html") != -1){
-        moveElement("preview",-540,0,5);
-      }
-      if (destination.indexOf("contact.html") != -1){
-        moveElement("preview",-720,0,5);
-      }
-    }
-  }
-}
-
-function showSection(id){
-  var sections = document.getElementsByTagName("section");
-  for (var i=0; i<sections.length; i++){
-    if (sections[i].getAttribute("id") != id) {
-      sections[i].style.display = "none";
-    }
-    else{
-      sections[i].style.display = "block";
-    }
-  }
-}
-
-function prepareInternalnav(){
-  if (!document.getElementById) return false;
-  if (!document.getElementsByTagName) return false;
-  var articles = document.getElementsByTagName("article");
-  if (articles.length == 0) return false;
-  var navs = articles[0].getElementsByTagName("nav");
-  if (navs.length == 0) return false;
-  var nav = navs[0];
-  var links = nav.getElementsByTagName("a");
-  for (var i=0; i<links.length; i++){
-    var sectionId = links[i].getAttribute("href").split("#")[1];
-    if (!document.getElementById(sectionId)) continue;
-    document.getElementById(sectionId).style.display = "none";
-    links[i].destination = sectionId;
-    links[i].onclick = function(){
-      showSection(this.destination);
+function getHTTPObject(){
+  if (typeof XMLHttpRequest == "undefined")
+    XMLHttpRequest = function(){
+      try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+      catch (e) {}
+      try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+      catch (e) {}
+      try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+      catch (e) {}
       return false;
     }
+    return new XMLHttpRequest();
   }
-}
-
-addLoadEvent(highlightPage);
-addLoadEvent(prepareSlideshow);
-addLoadEvent(prepareInternalnav);
